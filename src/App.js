bd51e1c9df06838d5edea90f7a4a807bdbeaccef
Ritik Mahakalkar <153834897+Ritik-Mahakalkar@users.noreply.github.com>
2025-03-11 16:56:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginpopup from './Pages/Loginpopup/Loginpopup'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Loginpopup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Loginpopup/Loginpopup.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import './Loginpopup.css'
+
+const Loginpopup = () => {
+    const[forgetPasswordState, setForgetPasswordState]=useState(false);
+    const [currentState,setCurrentState]=useState("Login");
+
+    if(forgetPasswordState){
+      return(
+        <div className='login-popup'>
+        <form action="" className="login-popup-container">
+
+            <div className="login-popup-title">
+                <h2>Forgot </h2>
+               
+            </div>
+            <div className="login-popup-input">
+                <input type="email" placeholder='emali' required />
+                <input type="password" placeholder='Previous password' required />
+                <input type="password" placeholder='New password' required />
+            </div>
+            <button>Forgot Password</button>
+             <div className="login-popup-condition">
+                <input type="checkbox" required />
+                <p className='para'>I aggree terms and condition and privacy policy.</p>
+             </div>
+             <div className='subtag'>
+             {<p className='para '>Have already account?<span onClick={()=>window.location.reload()}>Login here</span></p>}
+             </div>
+             
+             
+        </form>
+      
+    </div>
+
+      )
+    }
+  return (
+    <div className='login-popup'>
+        <form action="" className="login-popup-container">
+
+            <div className="login-popup-title">
+                <h2>{currentState}</h2>
+               
+            </div>
+            <div className="login-popup-input">
+                {currentState==="Login"?<></>:<input type="text" placeholder='Name' required />}
+                
+                <input type="email" placeholder='emali' required />
+                <input type="password" placeholder='password' required />
+            </div>
+            <button>{currentState==="Sign Up"?"Create account":"Login"}</button>
+             <div className="login-popup-condition">
+                <input type="checkbox" required />
+                <p className='para'>I aggree terms and condition and privacy policy.</p>
+             </div>
+             <div className='subtag'>
+             {currentState==="Login"?<p className='para '>Create account?<span onClick={()=>setCurrentState("Sign Up")}>Click here</span> OR <span onClick={()=>setForgetPasswordState(true)}>Forgot Password</span> </p>:<p className='para'> Have already account?<span onClick={()=>setCurrentState("Login")}>Login here</span></p>}
+             </div>
+             
+             
+        </form>
+      
+    </div>
+  )
+}
+
+export default Loginpopup
